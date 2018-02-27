d463f785919bc4314a39db4d55750f43d6800d59
Afriza <afrizaf3@gmail.com>
2018-02-27 16:13:15 +0700

    lanjutan

---
+++ pertemuan-1/tugas-web-1.php
@@ -0,0 +1,15 @@
+<?php
+
+// nama : Afriza Fadilah
+// nim	: 16090045
+// kelas: 4D
+
+$nama = "Afriza Fadilah";
+$jurusan = "Teknik Informatika";
+$matakuliah = "Web Programing 1";
+
+echo "Nama \t\t: $nama <br>";
+echo "Jurusan \t: $jurusan <br>";
+echo "Mata Kuliah \t: $matakuliah <br>";
+
+?>
