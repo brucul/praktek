a421d87038aec70e0707acc0ef4a545886a32388
Afriza <afrizaf3@gmail.com>
2018-02-27 17:09:29 +0700

    pertemuan-2

---
+++ pertemuan-2/array-multi.php
@@ -0,0 +1,17 @@
+<?php
+$data = array(
+	"web-prog" =>
+		array(
+			"Tamami" => 5,
+			"Andi" => 7,
+			"Yoga" => 9),
+	"kalkulus" =>
+		array(
+			"Yoga" => 6,
+			"Andi" => 8,
+			"Tamami" => 7)
+);
+
+echo "Nilai Web-Prog Andi : ".$data["web-prog"]["Andi"]."<br>";
+echo "Nilai Kalkulus Andi : ".$data["kalkulus"]["Andi"]."<br>";
+?>
---
+++ pertemuan-2/array.php
@@ -0,0 +1,11 @@
+<?php
+$data = array(
+	"nama" => "saya",
+	"usia" => 17,
+	"jomblo" => true
+);
+// . untuk menyatukan string
+echo "NAMA : ".$data["nama"]."<br>";
+echo "USIA : ".$data["usia"]."<br>";
+echo "JOMBLO ? ".$data["jomblo"]."<br>";
+?>
